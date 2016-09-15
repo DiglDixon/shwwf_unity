f6394821635e33aaa369a5b1997ae1816c591aae
DiglDixon <digl.design.nz@gmail.com>
2016-09-16 05:47:24 +0800

    Sequences fixed

--- Assets/Scripts/DisplaySequences/IntroSequence.cs
+++ Assets/Scripts/DisplaySequences/IntroSequence.cs
@@ -2,81 +2,31 @@
 using System.Collections;
 
 
-public class IntroSequence : MonoBehaviour{
+public class IntroSequence : SequenceWithBackground{
 
-	public GameObject[] showWhenStarted;
-	public SequenceObject background;
-	private SequenceElement[] sequences;
-	private int sequenceIndex;
+	public GameObject disableOnBackgroundEnter;
 
-	public GameObject[] hideOnceEntered;
-
-	public void Update(){
-		if (Input.GetKeyDown (KeyCode.I)) {
-			Begin ();
-		}
-	}
-
-	private void Awake(){
-		if (sequences == null)
-			FindSequences ();
-	}
-
-	private void FindSequences(){
-		sequences = GetComponentsInChildren<SequenceElement> ();
-		Diglbug.Log ("Found " + sequences.Length + " sequences");
-	}
-
-	private void OnEnable(){
-		if (sequences == null)
-			FindSequences ();
-		for (int k = 0; k < sequences.Length; k++) {
-			sequences [k].SequenceExitedEvent += SequenceExited;
-		}
+	protected override void OnEnable ()
+	{
+		base.OnEnable ();
 		background.SequenceEnteredEvent += BackgroundEntered;
 	}
 
-	private void OnDisable(){
-		if (sequences == null)
-			FindSequences ();
-		for (int k = 0; k < sequences.Length; k++) {
-			sequences [k].SequenceExitedEvent -= SequenceExited;
-		}
+	protected override void OnDisable ()
+	{
+		base.OnEnable ();
 		background.SequenceEnteredEvent -= BackgroundEntered;
 	}
 
-	public void Begin(){
-		for (int k = 0; k < showWhenStarted.Length; k++) {
-			showWhenStarted [k].SetActive (true);
-		}
-		sequenceIndex = 0;
-		RunSequenceAtIndex (0);
-	}
-
 	private void BackgroundEntered(){
-		sequenceIndex = 1;
-		RunSequenceAtIndex (1);
-		for (int k = 0; k < hideOnceEntered.Length; k++) {
-			hideOnceEntered [k].SetActive (false);
-		}
+		disableOnBackgroundEnter.SetActive (false);
 	}
 
-	private void SequenceExited(){
-		if (sequenceIndex == sequences.Length - 1) {
-			EntireSequenceFinished ();
-		} else {
-			sequenceIndex++;
-			RunSequenceAtIndex (sequenceIndex);
-		}
+	public override void SkipSequence (){
+		base.SkipSequence ();
+		BackgroundEntered ();
 	}
 
-	private void RunSequenceAtIndex(int index){
-		sequences [index].BeginSequence ();
-	}
-
-	private void EntireSequenceFinished(){
-		background.EndSequence();
-	}
 
 
 }
