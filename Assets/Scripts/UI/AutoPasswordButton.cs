57c26499ffed954171e4b06baa064a3f5c5b1ce0
DiglDixon <digl.design.nz@gmail.com>
2016-09-17 06:41:17 +0800

    AutoPasswords, disabled Pat's buttons

---
+++ Assets/Scripts/UI/AutoPasswordButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Button))]
+public class AutoPasswordButton : MonoBehaviour {
+
+	public Text autoPassword;
+	public PasswordControls passwordControls;
+
+	public void ButtonPressed(){
+		passwordControls.PasswordAttempted (autoPassword.text);
+	}
+
+}
--- Assets/Scripts/UI/PasswordControls.cs
+++ Assets/Scripts/UI/PasswordControls.cs
@@ -4,8 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class PasswordControls : MonoBehaviour{
 
-	public Text invalidPasswordText;
-
 	public void PasswordAttempted(string password){
 		ShowMode.Instance.PasswordEntered (password);
 		if (ShowMode.Instance.PasswordIsValid (password)) {
@@ -18,22 +16,6 @@ public class PasswordControls : MonoBehaviour{
 	private void PasswordAttemptFailed(string password){
 		// do things
 		Diglbug.Log ("Password attempt failed: "+password);
-	}
-
-	public void EnterDirectorMode(){
-		SceneManager.LoadScene ("director");
-	}
-
-	public void EnterActorMode(){
-		SceneManager.LoadScene ("actor");
-	}
-
-	public void EnterAudienceMode(){
-		SceneManager.LoadScene ("setup");
-	}
-
-	public void EnterGuideMode(){
-		SceneManager.LoadScene ("setup");
 	}
 
 }
